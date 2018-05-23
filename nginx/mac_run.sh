41d6b78539e06556dd69541dfaf543ca6a51e46b
xiepeng <xiepeng@5173.com>
2018-05-23 22:15:50 +0800

    update nginx config

--- nginx/mac_run.sh
+++ nginx/mac_run.sh
@@ -1,12 +1,19 @@
-#!/usr/bin/env bash -ex
+#!/bin/bash -ex
 
 #================================
 # mac os script
 #================================
+
 #nginx test config start
 sudo /usr/local/nginx/sbin/nginx -t -c /Users/xiepeng/Documents/mac_script/nginx/nginx_base.conf
+sudo /usr/local/nginx/sbin/nginx -c /Users/xiepeng/Documents/mac_script/nginx/nginx_base.conf
 
+
+sudo /usr/local/nginx/sbin/nginx -t -c /Users/xiepeng/Documents/mac_script/nginx/nginx_xw.conf
+sudo /usr/local/nginx/sbin/nginx -c /Users/xiepeng/Documents/mac_script/nginx/nginx_xw.conf
 #nginx stop
+ps aux|grep nginx
+kill -9 pid
 
 #nginx restart
 sudo /usr/local/nginx/sbin/nginx -s reload
